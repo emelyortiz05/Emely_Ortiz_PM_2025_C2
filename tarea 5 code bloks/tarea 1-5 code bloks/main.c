0e5b89d92a6641e57b8000af9ddc03ef6fc426c3
emelyortiz05 <emelycelinette@gmail.com>
2025-05-24 01:12:33 -0400

    Delete tarea 5 code bloks/tarea 1-5 code bloks directory

--- tarea 5 code bloks/tarea 1-5 code bloks/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/* Programa 1.1
-El siguiente es el primer programa escrito en el lenguaje C. */
-
-void main (void)
-{
-    printf("Mi primer programa en C");}
